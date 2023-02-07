6ae34a2aa5ca3086655ba3a4864233eec459b403
Olojede Joshua <famous.jt33@gmail.com>
2023-02-07 17:43:30 +0100

    feat: Add Deployment Script

--- week_2/github_deploy.py
+++ week_2/github_deploy.py
@@ -1,13 +1,14 @@
 from prefect.filesystems import GitHub
 from prefect.deployments import Deployment
+from etl_web_to_gcs import etl_parent_flow
 
 github_block = GitHub.load("de-zoomcamp-github")
 
 github_dep = Deployment.build_from_flow(
     flow=etl_parent_flow,
-    name="github-flow",
-    storage=github_block
-    path="week_1"
+    name="github-etl-deployment",
+    storage=github_block,
+    entrypoint="week_2/etl_web_to_gcs.py:etl_parent_flow",
     work_queue_name="default",
 )
 
