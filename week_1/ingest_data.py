9b6633837b12cdcd50023c48c083cac75fcce0a1
Olojede Joshua <famous.jt33@gmail.com>
2023-02-09 01:03:05 +0100

    feat: Add Ingest Data Script

--- week_1/ingest_data.py
+++ week_1/ingest_data.py
@@ -28,8 +28,8 @@ def main(params):
 
     df = next(df_iter)
 
-    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
-    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    # df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    # df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
